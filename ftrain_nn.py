a187499814ea4fc38b3a899081d4a619ecf8ba79
titanfall-I <45589677+titanfall-I@users.noreply.github.com>
2019-04-16 18:49:56 +0530

    Add files via upload

---
+++ ftrain_nn.py
@@ -0,0 +1,10 @@
+import train
+import numpy as np
+import sys
+
+X = np.load("data/X.npy")
+Y = np.load("data/Y.npy")
+
+params = np.load("params.npy").item()
+params, costs, iters = train.model(X, Y, params, [X.shape[0], 12, 4], 0.02, 100, True, int(sys.argv[1]))
+np.save("params.npy", params)
---
+++ init_params.py
@@ -0,0 +1,6 @@
+import numpy as np
+import train
+
+X = np.load("data/X.npy")
+params = train.init_params([X.shape[0], 12, 4])
+np.save("params.npy", params)
